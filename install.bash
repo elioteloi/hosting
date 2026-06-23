ec7298e72fddd22b5aa01546f8ddbc55e9870e44
Eliot Eloi <eliot.eloi22@gmail.com>
2026-06-23 20:38:29 -0300

    first part of the hosting compose file

---
+++ install.bash
@@ -0,0 +1,60 @@
+# bin/bash
+
+ARR=("plex" "jellyfin" "homeassistant")
+
+sudo apt install figlet
+
+echo "----------------------------------------------------------------------"
+
+figlet .env
+
+if [ ! -f ".env" ]; then
+    echo "Enter API key from VPN:"
+    read API_KEY
+    
+    echo API_KEY=$API_KEY > .env
+    echo ".env created with api key..."
+else 
+    echo ".env already created..."
+fi
+
+echo ""
+
+echo "----------------------------------------------------------------------"
+
+figlet config file
+
+for item in ${ARR[@]}
+do
+    if [ ! -d "./config-$item" ]; then
+        mkdir "config-$item"
+        echo "config-$item created..."
+    else
+        echo "config-$item already created..."
+    fi
+done
+
+echo "----------------------------------------------------------------------"
+
+echo ""
+
+figlet data file
+
+
+for item in ${ARR[@]}
+do
+    if [ ! -d "./data-$item" ]; then
+        mkdir "data-$item"
+        echo "data-$ created..."
+    else
+        echo "data-$item already created..."
+    fi
+done 
+
+echo "----------------------------------------------------------------------"
+
+echo ""
+
+figlet docker compose
+
+# docker compose up --build
